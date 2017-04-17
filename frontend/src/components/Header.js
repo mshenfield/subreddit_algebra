107ae3506fc788854761f08b55e78c7120475e5b
Max Shenfield <shenfieldmax@gmail.com>
2017-04-16 23:25:50 -0500

    Added SVG logo and favicon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
+import Logo from './Logo';
 
 import './Header.css';
 
 export default (props) => {
+  // 'Header__headers'
     let className = classNames(props.className)
 
     return (
       <div className="Header">
         {/*
-          The header should fill up the entire screen, but allow an inner
-          div that conforms to the App width.
+                    The header should fill up the entire screen, but allow an inner
+                    div that conforms to the App width.
         */}
         <div className={className}>
-          <h1>Subreddit Algebra</h1>
-          <h2>What's your favorite subreddit made of?</h2>
+          <div className="Header__logo">
+            <Logo/>
+          </div>
+          <div className="Header__headers">
+            <h1>Subreddit Algebra</h1>
+            <h2>What's your favorite subreddit made of?</h2>
+          </div>
         </div>
       </div>
     )
---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './Logo.css';
+
+export default (props) => {
+  return (
+    <svg className="Logo" viewBox="0 0 496 448">
+      <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" opacity="1">
+        <g id="Subreddit-Algebra-Icon" transform="translate(-252.000000, -119.000000)">
+          <g id="Group" transform="translate(226.000000, 135.000000)">
+            <g id="Atennae" transform="translate(275.000000, 0.000000)" stroke="#FFFFFF" strokeWidth="25">
+              <path d="M37.5,0.5 L0.5,121.5" id="Line" strokeLinecap="square"></path>
+              <path d="M41,1 L134.5,21.5" id="Line" strokeLinecap="square"></path>
+              <circle id="Oval-2" fill="#FFFFFF" cx="181.5" cy="34.5" r="33.5"></circle>
+            </g>
+            <g id="Noggin" transform="translate(0.000000, 122.000000)" fill="#FFFFFF">
+              <path d="M3.73928541,94.6958189 C3.73928541,94.6958189 9.19916543,34.9673377 64.0453163,34.9673377 C66.782702,34.9673377 69.3940753,35.1161234 71.8851959,35.3988429 C119.306067,40.7806802 123.151227,94.6958189 123.151227,94.6958189" id="Line" transform="translate(63.445256, 64.831578) rotate(-47.000000) translate(-63.445256, -64.831578) "></path>
+              <path d="M425.1809,95.2569519 C425.1809,95.2569519 430.59766,36.0001889 485.010652,36.0001889 C539.423644,36.0001889 543.649761,95.2569519 543.649761,95.2569519" id="Line" transform="translate(484.415331, 65.628570) rotate(48.000000) translate(-484.415331, -65.628570) "></path>
+              <ellipse id="Oval" cx="274" cy="158.5" rx="234" ry="151.5"></ellipse>
+            </g>
+            <path d="M375,350 C375,350 349.883846,381.127453 267.5,381.127453 C185.116154,381.127453 160,350 160,350" id="Line" stroke="#FF4500" strokeWidth="25" strokeLinecap="square"></path>
+            <path d="M199,241 L122,241" id="Line" stroke="#FF4500" strokeWidth="30" strokeLinecap="square"></path>
+            <g id="Plus-Eye" transform="translate(337.000000, 206.000000)" stroke="#FF4500" strokeWidth="30" strokeLinecap="square">
+              <path d="M37,66 L37,1" id="Line"></path>
+              <path d="M77,35 L0,35" id="Line"></path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
