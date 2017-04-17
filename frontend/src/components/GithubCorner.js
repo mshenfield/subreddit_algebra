ffb4cc9cc871df4fac4f767adc47f35632fe5298
Max Shenfield <shenfieldmax@gmail.com>
2017-04-17 08:51:04 -0500

    Make lighthouse happy with service workers

--- frontend/src/components/GithubCorner.js
+++ frontend/src/components/GithubCorner.js
@@ -23,7 +23,7 @@ export default (props) => {
   const target = targetBlank ? "_blank" : "_self"
 
   return (
-    <a href={url} className="github-corner" target={target} aria-label="View source on Github">
+    <a href={url} className="github-corner" rel="noopener" target={target} aria-label="View source on Github">
       <svg className="github-corner__svg" viewBox="0 0 250 250" style={style} aria-hidden="true">
         <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
         <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill={octocatColor} style={{transformOrigin: '130px 106px'}} className="octo-arm"></path>
