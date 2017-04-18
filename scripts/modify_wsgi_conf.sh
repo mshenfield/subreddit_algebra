813e95e313129c493efcfa505b753243fb23fb6f
Max Shenfield <shenfieldmax@gmail.com>
2017-04-17 22:04:49 -0500

    Fixed redirection from http to https

--- scripts/modify_wsgi_conf.sh
+++ scripts/modify_wsgi_conf.sh
@@ -1,4 +1,9 @@
-# We want Flask to be mounted at the /api url. Change the wsgi.conf file Elastic Beanstalk includes in the ondeck
-# directory.
-sed -i 's~WSGIScriptAlias /~WSGIScriptAlias /api~' ../wsgi.conf
-sed -i "/WSGIProcessGroup wsgi/a WSGIImportScript /opt/python/current/app/subreddit_algebra_app.wsgi process-group=wsgi application-group=%{GLOBAL}" ../wsgi.conf
+# We want Flask to be mounted at the /api url. Change the ELB default WSGIScriptAlias "/" to "/api"
+conf="../wsgi.conf"
+# We're using a custom delimeter "~" to avoid having to escape "/"
+sed -i 's~WSGIScriptAlias /~WSGIScriptAlias /api~' $conf
+
+# Preload the wsgi script, otherwise it loads on first visit after release, causing timeouts for that user
+# If not in wsgi.conf, I get an error trying to accesss the "wsgi" process group.
+script="/opt/python/current/app/subreddit_algebra_app.wsgi"
+sed -i "/WSGIProcessGroup/a WSGIImportScript $script process-group=wsgi application-group=%{GLOBAL}" $conf
