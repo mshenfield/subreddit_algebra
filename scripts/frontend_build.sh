ab23cd9367a8de1276bc84ae2f24722ab422992b
Max Shenfield <shenfieldmax@gmail.com>
2017-04-18 18:24:26 -0500

    Preserve enviornment on build

--- scripts/frontend_build.sh
+++ scripts/frontend_build.sh
@@ -4,13 +4,13 @@ set -e
 
 # Get 6.x LTS from nodesource
 curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
-sudo yum install -y nodejs
+yum install -y nodejs
 
 # yum install yarn, per https://yarnpkg.com/en/docs/install#linux-tab
-sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-sudo yum install -y yarn
+wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+yum install -y yarn
 
 # cd into the frontend directory, install required dependencies, and run build
 cd frontend
-sudo yarn install
-sudo yarn build
+yarn install
+yarn build
