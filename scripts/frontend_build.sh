85174da5ef50cfac3f2092a694840d7975117cdc
Max Shenfield <shenfieldmax@gmail.com>
2017-04-19 04:44:12 -0500

    Handle issue with webpack that caused different filenames to be generated for different directories.
    Note that amazon puts these deployments in numbered bundles. There is no guarantee that all instances will be on the same number bundle, and they usually aren't.
    
    This also adds a healthcheck to make sure we don't have instances come online before they're ready

--- scripts/frontend_build.sh
+++ scripts/frontend_build.sh
@@ -10,7 +10,18 @@ yum install -y nodejs
 wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 yum install -y yarn
 
-# cd into the frontend directory, install required dependencies, and run build
-cd frontend
+# See https://github.com/webpack/webpack/issues/1479#issuecomment-158194161
+# Webpack, the underlying library used by create-react-app for all this magic,
+# has various dependencies that cause it to generate a different has for some
+# applications depending on their filepath.
+
+# To avoid this, place the frontend in a consistent directory before building
+TEMP_FRONTEND=/tmp/frontend
+rm -r -f $TEMP_FRONTEND && mkdir $TEMP_FRONTEND && cp -R frontend /tmp && cd $TEMP_FRONTEND
 yarn install
 yarn build
+
+# Copy result to /var/www/html
+SERVER_ROOT="/var/www/html"
+rm -r -f $SERVER_ROOT/*
+cp --recursive $TEMP_FRONTEND/build/* $SERVER_ROOT
